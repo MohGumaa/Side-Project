05db0fc6935aabb38b43531cf5ac05c4c3a9b4fe
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-02-21 20:28:31 +0400

    password copy to clipboard

--- pw.py
+++ pw.py
@@ -6,9 +6,15 @@ PASSWORD = {'email': 'kljaglajlgjaghakj21',
             'twitter': 'nvnxv,x#@#42sfl'
 }
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1]
+
+if account in PASSWORD:
+    pyperclip.copy(PASSWORD[account])
+    print(f"Password for {account} copied to clipboard.")
+else:
+    print("There no account named {account}")
