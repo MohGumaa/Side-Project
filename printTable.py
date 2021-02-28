96b74459efb20db5964c641c4ad04e9a418ed15b
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-02-28 07:18:32 +0400

    add printTable.py project

---
+++ printTable.py
@@ -0,0 +1,11 @@
+#! python3
+# printTable.py display list of list in well organized table
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']
+
+            ]
+
+print(len(tableData))
+# print(dir(tableData))
