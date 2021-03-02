0b72fce4e1c4695fc20086beb460898a926688a2
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-03-02 07:18:34 +0400

    create function to print table format

--- printTable.py
+++ printTable.py
@@ -1,11 +1,22 @@
 #! python3
 # printTable.py display list of list in well organized table
 
+'''
+    Fucntion Take any list and display it in clean table format
+    by adding right padding width to item
+'''
+def printTableFormat(listItem, paddingWidth):
+    print('Table'.rjust(paddingWidth))
+    for row in range(len(listItem[0])):
+        for col in range(len(listItem)):
+            print(tableData[col][row].rjust(paddingWidth), end='')
+
+        print()
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']
 
             ]
 
-print(len(tableData))
-# print(dir(tableData))
+printTableFormat(tableData, 8)
