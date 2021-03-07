467baa09156d12d2aa5a487071c46e68a614258e
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-03-07 21:47:18 +0400

    Print Data In nice table format

--- printTable.py
+++ printTable.py
@@ -6,17 +6,37 @@
     by adding right padding width to item
 '''
 def printTableFormat(listItem, paddingWidth):
-    print('Table'.rjust(paddingWidth))
+    print('Table'.rjust(8))
     for row in range(len(listItem[0])):
         for col in range(len(listItem)):
-            print(tableData[col][row].rjust(paddingWidth), end='')
+            print(tableData[col][row].rjust(paddingWidth[col]), end=' ')
+
 
         print()
 
+'''
+    Function to find longest string in inner list
+'''
+
+def longestString(listItem):
+    colWidths = []
+
+    for items in listItem:
+        max = 0
+        # Loop through inner list to find longest item
+        for item in items:
+            if len(item) > max:
+                max = len(item)
+        colWidths.append(max)
+    return colWidths
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
-             ['dogs', 'cats', 'moose', 'goose']
-
+             ['dogs', 'cats', 'moose', 'goose'],
+             ['name', 'ali', 'majed', 'omer']
             ]
 
-printTableFormat(tableData, 8)
+
+
+colWidths = longestString(tableData)
+printTableFormat(tableData, colWidths)
